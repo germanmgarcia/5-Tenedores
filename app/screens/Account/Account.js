61ecdd12d174ea1cf23995c379e8e30e904fd73f
Germán <gmgarcia@ucundinamarca.edu.co>
2021-01-28 13:06:44 -0500

    Añadiendo Loading a la Screen de Account

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Text } from "react-native";
 import * as firebase from "firebase";
+import Loading from "../../components/Loading";
 import UserGuest from "./UserGuest";
 import UserLogged from "./UserLogged";
 
@@ -13,7 +13,7 @@ export default function Account() {
     });
   }, []);
 
-  if (login === null) return <Text>Cargando...</Text>;
+  if (login === null) return <Loading isVisible={true} text="Cargando..." />;
 
   return login ? <UserLogged /> : <UserGuest />;
 }
