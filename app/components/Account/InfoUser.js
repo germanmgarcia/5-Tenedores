2e7c4c2c12b167e01fa8ec170d7b94b46019be0d
Germán <gmgarcia@ucundinamarca.edu.co>
2021-02-03 20:35:06 -0500

    Actualizando el avatar del usuario en firebase

--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -29,7 +29,7 @@ export default function InfoUser(props) {
       } else {
         uploadImage(result.uri)
           .then(() => {
-            console.log("Imagen Subida");
+            updatePhotoUrl();
           })
           .catch(() => {
             toastRef.current.show("Error al actualizar el avatar.");
@@ -43,6 +43,19 @@ export default function InfoUser(props) {
     const blob = await response.blob();
     const ref = firebase.storage().ref().child(`avatar/${uid}`);
     return ref.put(blob);
+  };
+
+  const updatePhotoUrl = () => {
+    firebase
+      .storage()
+      .ref(`avatar/${uid}`)
+      .getDownloadURL()
+      .then(async (response) => {
+        const update = {
+          photoURL: response,
+        };
+        await firebase.auth().currentUser.updateProfile(update);
+      });
   };
 
   return (
