cece6259009dad9033598bac553b4aa9f6aaca99
Germán <gmgarcia@ucundinamarca.edu.co>
2021-02-03 20:26:28 -0500

    Solucionar Warning "Setting a timer for a long period of time"

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from "react";
-import Navigation from "./app/navigations/Navigation";
+import { LogBox } from "react-native";
 import { firebaseApp } from "./app/utils/firebase";
+import Navigation from "./app/navigations/Navigation";
+
+LogBox.ignoreLogs(["Setting a timer"]);
 
 export default function App() {
   return <Navigation />;
