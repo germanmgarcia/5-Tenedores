328037eb14dc9bdca5f96afaabb935a2c217fde2
Germán <gmgarcia@ucundinamarca.edu.co>
2021-02-28 10:37:39 -0500

    Abriendo Google Map con la localización del usuario

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -12,6 +12,7 @@ import { map, size, filter } from "lodash";
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
 import * as Location from "expo-location";
+import MapView from "react-native-maps";
 import Modal from "../Modal";
 
 const widthScreen = Dimensions.get("window").width;
@@ -142,7 +143,24 @@ function Map(props) {
 
   return (
     <Modal isVisible={isVisibleMap} setIsVisible={setIsVisibleMap}>
-      <Text>Mapa</Text>
+      <View>
+        {location && (
+          <MapView
+            style={styles.mapStyle}
+            initialRegion={location}
+            showsUserLocation={true}
+            onRegionChange={(region) => setLocation(region)}
+          >
+            <MapView.Marker
+              coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+              }}
+              draggable
+            />
+          </MapView>
+        )}
+      </View>
     </Modal>
   );
 }
@@ -264,5 +282,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     height: 200,
     marginBottom: 20,
+  },
+  mapStyle: {
+    width: "100%",
+    height: 550,
   },
 });
