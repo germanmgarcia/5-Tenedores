7d55b76460293a4a66e23f398f15d7dfcaef19d3
Germán <gmgarcia@ucundinamarca.edu.co>
2021-03-01 16:31:56 -0500

    Obteniendo la información del restaurnate

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Restaurant(props) {
   const { navigation, route } = props;
   const { id, name } = route.params;
+  const [restaurant, setRestaurant] = useState(null);
 
   navigation.setOptions({ title: name });
+
+  useEffect(() => {
+    db.collection("restaurants")
+      .doc(id)
+      .get()
+      .then((response) => {
+        const data = response.data();
+        data.id = response.id;
+        setRestaurant(data);
+      });
+  }, []);
 
   return (
     <View>
