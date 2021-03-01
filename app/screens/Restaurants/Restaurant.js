69d51762b823b366c55c6a255b6042c71318d8c3
Germán <gmgarcia@ucundinamarca.edu.co>
2021-03-01 13:04:02 -0500

    Añadir el nombre del restaurante en el titulo

--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Restaurant() {
+export default function Restaurant(props) {
+  const { navigation, route } = props;
+  const { id, name } = route.params;
+
+  navigation.setOptions({ title: name });
+
   return (
     <View>
       <Text>Restaurante info....</Text>
