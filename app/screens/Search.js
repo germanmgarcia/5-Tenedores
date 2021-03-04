111a8f850e05d0ad73e2c6a04349f4279696e2b0
Germán <gmgarcia@ucundinamarca.edu.co>
2021-03-04 15:25:50 -0500

    Ejecutando la búsqueda en firestore

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, FlatList, Image, View, Text } from "react-native";
 import { SearchBar, ListItem, Icon } from "react-native-elements";
+import { FireSQL } from "firesql";
+import firebase from "firebase/app";
+
+const fireSQL = new FireSQL(firebase.firestore(), { includeId: "id" });
 
 export default function Search(props) {
   const { navigation } = props;
   const [search, setSearch] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+
+  console.log(restaurants);
+
+  useEffect(() => {
+    if (search) {
+      fireSQL
+        .query(`SELECT * FROM restaurants WHERE name LIKE '${search}%'`)
+        .then((response) => {
+          setRestaurants(response);
+        });
+    }
+  }, [search]);
 
   return (
     <View>
