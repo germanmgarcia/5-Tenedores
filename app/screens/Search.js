6b0414a7fe6e41bcc8ffe4d29c5985177de1fbc1
Germán <gmgarcia@ucundinamarca.edu.co>
2021-03-04 15:18:52 -0500

    Barra de busqueda

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,10 +1,25 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, FlatList, Image, View, Text } from "react-native";
+import { SearchBar, ListItem, Icon } from "react-native-elements";
 
-export default function Search() {
+export default function Search(props) {
+  const { navigation } = props;
+  const [search, setSearch] = useState("");
+
   return (
     <View>
-      <Text>Search...</Text>
+      <SearchBar
+        placeholder="Busca tu restaurante..."
+        onChangeText={(e) => setSearch(e)}
+        containerStyle={styles.searchBar}
+        value={search}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  searchBar: {
+    marginBottom: 0,
+  },
+});
